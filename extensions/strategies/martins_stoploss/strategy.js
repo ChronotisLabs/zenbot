af2b4d91a4e73151041914637d0b2d076abdf3e6
Martin Walker <martin@marsjupiter.com>
2017-09-28 06:27:44 +0000

    fix buy and sell states

--- extensions/strategies/martins_stoploss/strategy.js
+++ extensions/strategies/martins_stoploss/strategy.js
@@ -9,9 +9,9 @@ module.exports = function container (get, set, clear) {
     getOptions: function () {
       this.option('period', 'period length', String, '30m')
        this.option('min_periods', 'min. number of history periods', Number, 24)
-      this.option('loss_pct', 'How much can we lose' , Number, 1)
-      this.option('profit_pct', 'When to sell' , Number, 5)
-      this.option('buy_trigger_pct', 'When to buy' , Number, 0.5)
+      this.option('loss_pct', 'How much can we lose' , Number, 0.2)
+      this.option('profit_pct', 'When to sell' , Number, 1)
+      this.option('buy_trigger_pct', 'When to buy' , Number, 0.1)
       this.option('buy_trigger_length', 'Period to compare to' , Number, 0.5)
     },
 
@@ -19,13 +19,14 @@ module.exports = function container (get, set, clear) {
     // called every internal trading interval
     //
     calculate: function (s) {
-     if (s.lookback.length >= s.options.min_periods) {
-
-     } 
+     s.strategy.onPeriod.call(s.ctx,s, function () {
+                
+                }
+            )
     },
 
     onPeriod: function (s, cb) {
-
+      //console.log(s.strategy_state+ " "+ s.strategy_sell_price+" "+s.strategy_stoploss_price+" "+s.period_sell);
 
       if (typeof s.strategy_state === 'undefined')
       {
@@ -42,6 +43,7 @@ module.exports = function container (get, set, clear) {
 
           if (s.period.close > s.strategy_sell_price)
           {
+             s.strategy_state = 'looking'
              s.signal = 'sell'
              return cb()
           }
@@ -49,6 +51,7 @@ module.exports = function container (get, set, clear) {
 
           if (s.period.close < s.strategy_stoploss_price)
           {
+             s.strategy_state = 'looking'
              s.signal = 'sell'
              return cb()
           }
